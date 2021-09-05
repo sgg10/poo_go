ad14e3d07ba9d1a3776004c45db47f496f02b0ad
Sebastian Granda Gallego <sgg10.develop@gmail.com>
2021-09-05 00:09:30 -0500

    Herencia

--- main.go
+++ main.go
@@ -2,28 +2,40 @@ package main
 
 import "fmt"
 
-type Employee struct {
-	id       int
-	name     string
-	vacation bool
+type Person struct {
+	name string
+	age  int
 }
 
-func NewEmployee(id int, name string, vacation bool) *Employee {
-	fmt.Println("Creating a new employee...")
-	return &Employee{id: id, name: name, vacation: vacation}
+type Employee struct {
+	id        int
+	vacations bool
+}
+
+type FullTimeEmployee struct {
+	Person
+	Employee
+}
+
+// func NewFulltimeEmployee(id int, name string, age int, vacations bool) *FulltimeEmployee {
+// 	fte := FullTimeEmployee{}
+// 	fte.id = id
+// 	fte.name = name
+// 	fte.age = age
+// 	fte.vacations = vacations
+// 	return &fte
+// }
+
+func GetMessage(p Person) {
+	fmt.Printf("%s with age: %d\n", p.name, p.age)
 }
 
 func main() {
-	e := Employee{
-		id:       1,
-		name:     "Connor",
-		vacation: true,
-	}
-	fmt.Println(e)
-
-	e2 := new(Employee)
-	fmt.Println(*e2)
-
-	e3 := NewEmployee(2, "Ezio", true)
-	fmt.Println(*e3)
+	ftEmployee := FullTimeEmployee{}
+	ftEmployee.name = "name"
+	ftEmployee.age = 20
+	ftEmployee.id = 1
+	ftEmployee.vacations = true
+	fmt.Println(ftEmployee)
+	//fmt.Println(ftEmployee.)
 }
